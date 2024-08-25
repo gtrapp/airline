72dac174bc1f70582bee9e89d86c974cb38cbd3b
George Trapp <gtrapp@gmail.com>
2024-08-25 16:37:04 -0700

    Booking logic

--- flights/views.py
+++ flights/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from .models import Flight, Passenger
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
@@ -12,12 +11,11 @@ def index(request):
 
 def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
-    passengers = flight.flights_passengers.all()
-
+    passengers = flight.flights_passengers.all() # Passenger.flights -> related_name="flights_passengers". The link between Flight.id and Passenger.flights
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": passengers
-        "non_passengers": Passenger.objects.exclude(flight=flight).all()
+        "passengers": passengers,
+        "non_passengers": Passenger.objects.exclude(flights=flight).all() # property of Passenger.flights model
     })
 
 
@@ -27,5 +25,5 @@ def book(request, flight_id):
         passenger_id = int(request.POST["passenger"])  # Finding the passenger id from the submitted form data
         passenger = Passenger.objects.get(pk=passenger_id)  # Finding the passenger based on the id
         passenger.flights.add(flight)  # Add passenger to the flight
-        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))  # Redirect user to flight page
+        return HttpResponseRedirect(reverse("flight", args=(flight.id, )))  # Redirect user to flight page
     
