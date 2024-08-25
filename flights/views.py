6d5e1faa110492112b516fa79ffd9645ffbcafbb
George Trapp <gtrapp@gmail.com>
2024-08-25 00:05:11 -0700

    Passenger model created

--- flights/views.py
+++ flights/views.py
@@ -6,3 +6,12 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    passengers = flight.flights_passengers.all()
+
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers": passengers
+    })
