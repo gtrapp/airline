cab0620b01af5c706fa4db754af4bbd22dc238b8
George Trapp <gtrapp@gmail.com>
2024-08-27 03:10:57 -0400

    admin panel. user login logout.

--- flights/views.py
+++ flights/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Flight, Passenger
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
-def index(request):
+def index(request): 
     return render(request, "flights/index.html", {
-        "flights": Flight.objects.all()
-    })
+         "flights": Flight.objects.all()
+         })
 
 def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
@@ -27,3 +29,5 @@ def book(request, flight_id):
         passenger.flights.add(flight)  # Add passenger to the flight
         return HttpResponseRedirect(reverse("flight", args=(flight.id, )))  # Redirect user to flight page
     
+
+
